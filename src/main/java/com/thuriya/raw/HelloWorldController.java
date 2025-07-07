4b35f48ebbfcfcb1800508233e00a656d74d3ad3
Thuria234naing <113816550+Thuria234naing@users.noreply.github.com>
2025-07-07 15:32:41 +0630

    second commit

--- src/main/java/com/thuriya/raw/HelloWorldController.java
+++ src/main/java/com/thuriya/raw/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@GetMapping(path = "/hello")
 	public String HelloWorld(){
-		return "Hello World";
+		return "Hello World is fucking lame";
 	}
 }
