ea89d509b7ec405003754ca42dc0b536b0e5f9a8
Thuria234naing <113816550+Thuria234naing@users.noreply.github.com>
2025-07-13 13:46:17 +0630

    CRUD -D

--- src/main/java/com/thuriya/raw/job/JobController.java
+++ src/main/java/com/thuriya/raw/job/JobController.java
@@ -33,5 +33,13 @@ public class JobController {
 		}
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
+	@DeleteMapping("jobs/{id}")
+	public ResponseEntity<String> deleteJob(@PathVariable Long id){
+		boolean deleted = jobService.deleteJobById(id);
+		if (deleted){
+			return new ResponseEntity<> ("Job deleted successfully" , HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
 
 }
