53c5b71e09674796e4265db50eee2b7d34658e55
Thuria234naing <113816550+Thuria234naing@users.noreply.github.com>
2025-07-16 07:00:31 +0630

    commit

--- src/main/java/com/thuriya/raw/job/Job.java
+++ src/main/java/com/thuriya/raw/job/Job.java
@@ -1,12 +1,29 @@
 package com.thuriya.raw.job;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "job_table")
 public class Job {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private String description;
 	private String minSalary;
 	private String maxSalary;
 	private String location;
+	public Job() {
+	}
+
+	public Job(Long id, String title, String description, String minSalary, String maxSalary, String location) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.minSalary = minSalary;
+		this.maxSalary = maxSalary;
+		this.location = location;
+	}
 
 	public Long getId() {
 		return id;
@@ -55,13 +72,5 @@ public class Job {
 		return location;
 	}
 
-	public Job(Long id, String title, String description, String minSalary, String maxSalary, String location) {
-		this.id = id;
-		this.title = title;
-		this.description = description;
-		this.minSalary = minSalary;
-		this.maxSalary = maxSalary;
-		this.location = location;
-	}
 
 }
